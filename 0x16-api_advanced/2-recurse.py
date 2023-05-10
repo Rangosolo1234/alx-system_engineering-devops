f4d22995b4df6e6c1e3dd0b7518609761fd01425
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-05-09 19:11:11 -0700

    Recursive querry function

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+importing requests module =====
+"""
+
+from requests import get
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    function that queries the Reddit API using recursion
+    """
+
+    params = {'show': 'all'}
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return None
+
+    user_agent = {'User-agent': 'Google Chrome Version 112.0.5615.139'}
+
+    url = 'https://www.reddit.com/r/{}/hot/.json?after={}'.format(subreddit,
+                                                                  after)
+
+    response = get(url, headers=user_agent, params=params)
+
+    if (response.status_code != 200):
+        return None
+
+    all_data = response.json()
+
+    try:
+        firstraw = all_data.get('data').get('children')
+        after = all_data.get('data').get('after')
+
+        if after is None:
+            return hot_list
+
+        for i in firstraw:
+            hot_list.append(i.get('data').get('title'))
+
+        return recurse(subreddit, hot_list, after)
+    except:
+        print("None")
