798f8d5fb6323c072240d2701c22a4e863e02c42
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-05-14 13:12:48 -0700

    Debugging using puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+#Fixing the file typo
+exec { 'fix_typo':
+  command => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
+  path    => '/bin/'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+Cannot trace the problem?
